dbbcc4b27b8475074097cc3ff15f332dc71bbff7
Dodilanne <dodilanne@ada-anvil.io>
2025-08-08 13:15:33 -0400

    Add missing exports

--- packages/csl/src/index.ts
+++ packages/csl/src/index.ts
@@ -1,3 +1,5 @@
 export { signTransaction, type SignTransactionInput } from "./sign-transaction";
 export { parsePrivateKey } from "./parse-private-key";
 export { parseTransaction } from "./parse-transaction";
+export { addRequiredSigner, type AddRequiredSignerInput } from "./add-required-signer";
+export { parseEd25519KeyHash } from "./parse-ed25519-key-hash";
