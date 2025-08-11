540d7d1b8c71ed7a24c773e6c2d8ae7e7ff4d6cd
Dodilanne <dodilanne@ada-anvil.io>
2025-08-11 17:07:47 -0400

    Test sign data function

--- packages/cms/src/sign-data.ts
+++ packages/cms/src/sign-data.ts
@@ -1,3 +1,4 @@
+import type { ParsedAddress } from "@anvil-vault/csl";
 import { parseFromHex } from "@anvil-vault/utils";
 import {
   AlgorithmId,
@@ -11,12 +12,12 @@ import {
   Label,
   ProtectedHeaderMap,
 } from "@emurgo/cardano-message-signing-nodejs-gc";
-import type { Address, PrivateKey } from "@emurgo/cardano-serialization-lib-nodejs-gc";
+import type { PrivateKey } from "@emurgo/cardano-serialization-lib-nodejs-gc";
 import { type Result, parseError, unwrap } from "trynot";
 
 export type SignDataInput = {
   payload: string | Buffer;
-  address: Address;
+  address: ParsedAddress;
   privateKey: PrivateKey;
   externalAad?: string | Buffer;
 };
@@ -35,7 +36,29 @@ export function signData(input: SignDataInput): Result<SignDataOutput> {
     const payload = unwrap(parseFromHex(input.payload));
     const externalAad = unwrap(parseFromHex(input.externalAad));
 
+    const paymentCred = input.address.payment_cred();
+    if (paymentCred.has_script_hash()) {
+      return new Error("Can't sign data with script address");
+    }
+
+    const paymentHash = paymentCred.to_keyhash();
+    if (!paymentHash) {
+      return new Error("Can't get payment hash from address");
+    }
+
+    if (paymentHash.to_hex() !== privateKey.to_public().hash().to_hex()) {
+      return new Error("Private key doesn't match the address");
+    }
+
     const protectedHeaders = HeaderMap.new();
+
+    protectedHeaders.set_algorithm_id(Label.from_algorithm_id(AlgorithmId.EdDSA));
+    protectedHeaders.set_key_id(input.address.to_address().to_bytes());
+    protectedHeaders.set_header(
+      Label.new_text("address"),
+      CBORValue.new_bytes(input.address.to_address().to_bytes()),
+    );
+
     const protectedSerialized = ProtectedHeaderMap.new(protectedHeaders);
     const unprotected = HeaderMap.new();
     const headers = Headers.new(protectedSerialized, unprotected);
@@ -52,6 +75,7 @@ export function signData(input: SignDataInput): Result<SignDataOutput> {
 
     const coseKey = COSEKey.new(Label.from_key_type(KeyType.OKP));
     coseKey.set_algorithm_id(Label.from_algorithm_id(AlgorithmId.EdDSA));
+    coseKey.set_key_id(input.address.to_address().to_bytes());
     coseKey.set_header(Label.new_int(Int.new_i32(-1)), CBORValue.new_int(Int.new_i32(6)));
     coseKey.set_header(
       Label.new_int(Int.new_i32(-2)),
--- packages/csl/src/index.ts
+++ packages/csl/src/index.ts
@@ -1,3 +1,3 @@
 export { addRequiredSigner, type AddRequiredSignerInput } from "./add-required-signer";
-export { parseAddress, type ParseAddressInput } from "./parse-address";
+export { parseAddress, type ParsedAddress, type ParseAddressInput } from "./parse-address";
 export { signTransaction, type SignTransactionInput } from "./sign-transaction";
