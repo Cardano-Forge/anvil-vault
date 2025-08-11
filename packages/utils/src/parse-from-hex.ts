529105d3fb89c56bb75fcefe5a4c523bf08ab3c6
Dodilanne <dodilanne@ada-anvil.io>
2025-08-11 12:25:31 -0400

    Allow undefined and default constructor in parse from hex function

--- packages/utils/src/parse-from-hex.ts
+++ packages/utils/src/parse-from-hex.ts
@@ -1,13 +1,33 @@
 import { type Result, parseError } from "trynot";
 
+// biome-ignore lint/suspicious/noExplicitAny: don't care about the constructor if input is always undefined
+export function parseFromHex(hexOrInstance: undefined, HexConstuctor?: any): undefined;
+export function parseFromHex(hexOrInstance: Buffer | string): Result<Buffer>;
+export function parseFromHex(
+  hexOrInstance: Buffer | string | undefined,
+): Result<Buffer | undefined>;
 export function parseFromHex<T>(
   hexOrInstance: T | string,
   HexConstructor: { from_hex: (hex: string) => T },
-): Result<T> {
+): Result<T>;
+export function parseFromHex<T>(
+  hexOrInstance: T | string | undefined,
+  HexConstructor: { from_hex: (hex: string) => T },
+): Result<T | undefined>;
+export function parseFromHex<T>(
+  hexOrInstance: T | string | undefined,
+  HexConstructor?: { from_hex: (hex: string) => T },
+): Result<T | undefined> {
+  if (hexOrInstance === undefined) {
+    return undefined;
+  }
   if (typeof hexOrInstance !== "string") {
     return hexOrInstance;
   }
   try {
+    if (HexConstructor === undefined) {
+      return Buffer.from(hexOrInstance, "hex") as T;
+    }
     return HexConstructor.from_hex(hexOrInstance);
   } catch (error) {
     return parseError(error);
